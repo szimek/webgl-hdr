5985f19fb6be788bd88d73de742199ea39afe244
Szymon Nowak <szimek@gmail.com>
2011-01-30 23:36:03 +0100

    Working Durand02 tmo using filters.

--- javascripts/App.js
+++ javascripts/App.js
@@ -2,60 +2,25 @@ var statsEnabled = true;
 
 var container, stats;
 
-var camera, mesh, scene, renderer;
+var renderer;
 
-// Textures
-var imageTexture,
-    hdrTexture,
-    luminanceTexture,
-    bilateralTextureX, bilateralTextureY;
+var imageTexture;
 
 // Shader attributes
-var exposure = { max: 1.0, min: 0.0, step: 0.01, value: 0.2 }, kernel, blurX, blurY;
-
-// Materials
-var pngDecodeMaterial,
-    luminanceMaterial,
-    bilateralMaterial,
-    toneMappingMaterial;
+var exposure = { max: 1.0, min: 0.0, step: 0.01, value: 0.2 };
 
 // Filter
-var luminanceFilter,
-    pngFilter;
+var pngFilter,
+    luminanceFilter,
+    bilateralFilter,
+    durand02TMO;
 
 // Extensions
 var glExtFT;
 
 init();
 
-//
-// Gauss related stuff
-//
-function gauss(x, sigma) {
-    // return 1.0 / (sigma * Math.sqrt(2 * Math.PI)) * Math.exp( - (x * x) / (2.0 * sigma * sigma));
-    return Math.exp( - (x * x) / (2.0 * sigma * sigma));
-}
 
-function buildKernel(sigma) {
-    var kMaxKernelSize = 43,
-        kernelSize = 43, // 2 * Math.ceil( sigma * 3.0 ) + 1,
-        i;
-
-    if ( kernelSize > kMaxKernelSize ) kernelSize = kMaxKernelSize;
-    var halfWidth = ( kernelSize - 1 ) * 0.5;
-
-    var values = new Array( kernelSize );
-    var sum = 0.0;
-    for( i = 0; i < kernelSize; ++i ) {
-        values[ i ] = gauss( i - halfWidth, sigma );
-        sum += values[ i ];
-    }
-
-    // normalize the kernel
-    for( i = 0; i < kernelSize; ++i ) values[ i ] /= sum;
-
-    return values;
-}
 
 function init() {
     container = document.createElement( 'div' );
@@ -90,42 +55,18 @@ function init() {
         imageTexture.width = image.width;
         imageTexture.height = image.height;
 
-        // Camera
-        var halfX = image.width / 2,
-            halfY = image.height / 2;
-        camera = new THREE.Camera();
-        camera.projectionMatrix = THREE.Matrix4.makeOrtho( -halfX, halfX, halfY, -halfY, -1, 1 );
-        camera.position.z = 1;
-
-        // Mesh (without any material yet)
-        mesh = new THREE.Mesh( new Plane( image.width, image.height ) );
-
-        // Scene
-        scene = new THREE.Scene();
-        scene.addObject(mesh);
-
-        var shader, uniforms;
-        var renderTargetSettings = {
-            format: THREE.RGBFormat,
-            type: THREE.FloatType,
-            wrap_s: THREE.ClampToEdgeWrapping,
-            wrap_t: THREE.ClampToEdgeWrapping,
-            min_filter: THREE.NearestFilter,
-            mag_filter: THREE.NearestFilter
-        };
-
         // Renderer
         renderer = new THREE.WebGLRenderer( false );
         renderer.setSize( image.width, image.height );
         container.appendChild( renderer.domElement );
 
-        // Enable floating point texture support
+        // Enable floating point texture extension
         if ( !renderer.context.getExtension("OES_texture_float") ) {
             alert("Your browser doesn't support required OES_texture_float extension.");
             return;
         }
 
-        // WebGL Inspector extensions
+        // Enable 'WebGL Inspector' frame termination extension
         glExtFT = renderer.context.getExtension("GLI_frame_terminator");
 
         ShaderUtils.load(["vs/basic", "fs/png_decode", "fs/rgb2y", "vs/bilateral", "fs/bilateral", "fs/tmo/none", "fs/tmo/Durand02"], function (err, shaders) {
@@ -134,110 +75,11 @@ function init() {
                 return;
             }
 
-            //
-            // Setup stuff for decoding PNG HDR image
-            //
-            // Render target
-            hdrTexture = new THREE.RenderTarget( image.width, image.height, renderTargetSettings);
-
-            // Material
-            shader = {
-                uniforms: {
-                    tPNG: { type: "t", value: 0, texture: imageTexture }
-                },
-                vertex: shaders["vs/basic"],
-                fragment: shaders["fs/png_decode"]
-            };
-
-            pngDecodeMaterial = new THREE.MeshShaderMaterial({
-                    uniforms: shader.uniforms,
-                    vertex_shader: shader.vertex,
-                    fragment_shader: shader.fragment
-            });
-
-            //
-            // Setup stuff for luminance map calculations
-            //
-            // Render target
-            luminanceTexture = new THREE.RenderTarget( image.width, image.height, renderTargetSettings);
-
-            // Material
-            shader = {
-                uniforms: {
-                    tHDR: { type: "t", value: 0, texture: hdrTexture }
-                },
-                vertex: shaders["vs/basic"],
-                fragment: shaders["fs/rgb2y"]
-            };
-
-            luminanceMaterial = new THREE.MeshShaderMaterial({
-                uniforms: shader.uniforms,
-                vertex_shader: shader.vertex,
-                fragment_shader: shader.fragment
-            });
-
-            //
-            // Setup stuff for bilateral filter
-            //
-            // Kernel
-            var sigma  = 0.35 * Math.pow(1.6, 6);
-            kernel = buildKernel( sigma );
-            blurX = new THREE.Vector2( 1 / image.width, 0 );
-            blurY = new THREE.Vector2( 0, 1 / image.height );
-
-            // Render targets
-            bilateralTextureX = new THREE.RenderTarget(
-                image.width,
-                image.height,
-                renderTargetSettings
-            );
-
-            bilateralTextureY = new THREE.RenderTarget(
-                image.width,
-                image.height,
-                renderTargetSettings
-            );
-
-            // Material
-            shader = {
-                uniforms: {
-                    tLuminanceMap: { type: "t", value: 0, texture: null },
-                    uImageIncrement: { type: "v2", value: blurX },
-                    cKernel: { type: "fv1", value: kernel }
-                },
-                vertex: shaders["vs/bilateral"],
-                fragment: shaders["fs/bilateral"]
-            };
-
-            bilateralMaterial = new THREE.MeshShaderMaterial({
-                uniforms: shader.uniforms,
-                vertex_shader: shader.vertex,
-                fragment_shader: shader.fragment
-            });
-
-            //
-            // Setup stuff for tone-mapping
-            //
-            // Material
-            shader = {
-                uniforms: {
-                    tHDR: { type: "t", value: 0, texture: hdrTexture },
-                    tLuminanceMap: { type: "t", value: 1, texture: luminanceTexture },
-                    tBilateralMap: { type: "t", value: 2, texture: bilateralTextureY },
-                    fExposure: { type: "f", value: exposure.value }
-                },
-                vertex: shaders["vs/basic"],
-                fragment: shaders["fs/tmo/Durand02"]
-            };
-
-            toneMappingMaterial = new THREE.MeshShaderMaterial({
-                uniforms: shader.uniforms,
-                vertex_shader: shader.vertex,
-                fragment_shader: shader.fragment
-            });
-
+            // Setup filters
             pngFilter = new app.filters.PNGHDRDecode(imageTexture, shaders);
-            luminanceFilter = new app.filters.Grayscale(hdrTexture, shaders);
+            luminanceFilter = new app.filters.Grayscale(pngFilter.renderTarget, shaders);
+            bilateralFilter = new app.filters.Bilateral(luminanceFilter.renderTarget, shaders);
+            durand02TMO = new app.filters.Durand02TMO(pngFilter.renderTarget, luminanceFilter.renderTarget, bilateralFilter.renderTarget, shaders);
 
             // Render loop
             setInterval( loop, 1000 / 60);
@@ -249,33 +91,12 @@ function init() {
 }
 
 function loop() {
-    // Decode PNG HDR
-    mesh.materials = [pngDecodeMaterial];
-    renderer.render( scene, camera, hdrTexture );
-    // hdrTexture = pngFilter.process(renderer);
+    pngFilter.process(renderer);
+    luminanceFilter.process(renderer);
+    bilateralFilter.process(renderer);
+    durand02TMO.process(renderer, true);
 
-    // Calculate luminance
-    // mesh.materials = [luminanceMaterial];
-    // renderer.render( scene, camera, luminanceTexture );
-    luminanceTexture = luminanceFilter.process(renderer);
-
-    // Horizontal bilateral pass
-    mesh.materials = [ bilateralMaterial ];
-    bilateralMaterial.uniforms.tLuminanceMap.texture = luminanceTexture;
-    bilateralMaterial.uniforms.uImageIncrement.value = blurX;
-    renderer.render( scene, camera, bilateralTextureX );
-
-    // Vertical bilateral pass
-    bilateralMaterial.uniforms.tLuminanceMap.texture = bilateralTextureX;
-    bilateralMaterial.uniforms.uImageIncrement.value = blurY;
-    renderer.render( scene, camera, bilateralTextureY );
-
-    // Perform tone mapping of HDR image
-    toneMappingMaterial.uniforms[ "fExposure" ].value = exposure.value;
-    mesh.materials = [toneMappingMaterial];
-    renderer.render( scene, camera );
-
-    // Mark frame end for WebGL Inspector
+    // Mark end of frame for WebGL Inspector
     if ( glExtFT ) glExtFT.frameTerminator();
 
     if ( statsEnabled ) stats.update();
--- javascripts/Filters.js
+++ javascripts/Filters.js
@@ -24,9 +24,9 @@ app.Filter.prototype.setup = function (image) {
 
     this.material = null;
 };
-app.Filter.prototype.process = function(renderer) {
-    renderer.render(this.scene, this.camera, this.renderTarget);
-    return this.renderTarget;
+app.Filter.prototype.process = function(renderer, renderToScreen) {
+    var target = renderToScreen ? undefined : this.renderTarget;
+    renderer.render(this.scene, this.camera, target);
 };
 
 
@@ -50,8 +50,11 @@ app.filters.PNGHDRDecode = function (texture, shaders) {
         vertex_shader: shader.vertex,
         fragment_shader: shader.fragment
     });
+
+    this.mesh.materials = [this.material];
 };
 app.filters.PNGHDRDecode.prototype = new app.Filter();
+
 
 app.filters.Grayscale = function (texture, shaders) {
     app.Filter.call(this);
@@ -75,3 +78,113 @@ app.filters.Grayscale = function (texture, shaders) {
     this.mesh.materials = [this.material];
 };
 app.filters.Grayscale.prototype = new app.Filter();
+
+
+app.filters.Bilateral = function (texture, shaders) {
+    app.Filter.call(this);
+
+    this.setup(texture);
+
+    this.inputTexture = texture;
+
+    this.renderTargetTemp = new THREE.RenderTarget( texture.width, texture.height, {
+        format: THREE.RGBFormat,
+        type: THREE.FloatType,
+        wrap_s: THREE.ClampToEdgeWrapping,
+        wrap_t: THREE.ClampToEdgeWrapping,
+        min_filter: THREE.NearestFilter,
+        mag_filter: THREE.NearestFilter
+    });
+
+    var sigma  = 0.35 * Math.pow(1.6, 6),
+        kernel = buildKernel( sigma );
+
+    this.blurX = new THREE.Vector2( 1 / texture.width, 0 );
+    this.blurY = new THREE.Vector2( 0, 1 / texture.height );
+
+    var shader = {
+        uniforms: {
+            tLuminanceMap: { type: "t", value: 0, texture: texture },
+            uImageIncrement: { type: "v2", value: this.blurX },
+            cKernel: { type: "fv1", value: kernel }
+        },
+        vertex: shaders["vs/bilateral"],
+        fragment: shaders["fs/bilateral"]
+    };
+
+    this.material = new THREE.MeshShaderMaterial({
+        uniforms: shader.uniforms,
+        vertex_shader: shader.vertex,
+        fragment_shader: shader.fragment
+    });
+
+    this.mesh.materials = [this.material];
+};
+app.filters.Bilateral.prototype = new app.Filter();
+app.filters.Bilateral.prototype.process = function (renderer) {
+    // Horizontal pass
+    this.material.uniforms.tLuminanceMap.texture = this.inputTexture;
+    this.material.uniforms.uImageIncrement.value = this.blurX;
+    renderer.render( this.scene, this.camera, this.renderTargetTemp );
+
+    // Vertical pass
+    this.material.uniforms.tLuminanceMap.texture = this.renderTargetTemp;
+    this.material.uniforms.uImageIncrement.value = this.blurY;
+    renderer.render( this.scene, this.camera, this.renderTarget );
+};
+
+
+app.filters.Durand02TMO = function(hdrTexture, luminanceTexture, bilateralTexture, shaders) {
+    app.Filter.call(this);
+
+    this.setup(hdrTexture);
+
+    var shader = {
+        uniforms: {
+            tHDR: { type: "t", value: 0, texture: hdrTexture },
+            tLuminanceMap: { type: "t", value: 1, texture: luminanceTexture },
+            tBilateralMap: { type: "t", value: 2, texture: bilateralTexture },
+            fExposure: { type: "f", value: 0.1 }
+        },
+        vertex: shaders["vs/basic"],
+        fragment: shaders["fs/tmo/Durand02"]
+    };
+
+    this.material = new THREE.MeshShaderMaterial({
+        uniforms: shader.uniforms,
+        vertex_shader: shader.vertex,
+        fragment_shader: shader.fragment
+    });
+
+    this.mesh.materials = [this.material];
+};
+app.filters.Durand02TMO.prototype = new app.Filter();
+
+//
+// Gauss related stuff
+//
+function gauss(x, sigma) {
+    // return 1.0 / (sigma * Math.sqrt(2 * Math.PI)) * Math.exp( - (x * x) / (2.0 * sigma * sigma));
+    return Math.exp( - (x * x) / (2.0 * sigma * sigma));
+}
+
+function buildKernel(sigma) {
+    var kMaxKernelSize = 43,
+        kernelSize = 43, // 2 * Math.ceil( sigma * 3.0 ) + 1,
+        i;
+
+    if ( kernelSize > kMaxKernelSize ) kernelSize = kMaxKernelSize;
+    var halfWidth = ( kernelSize - 1 ) * 0.5;
+
+    var values = new Array( kernelSize );
+    var sum = 0.0;
+    for( i = 0; i < kernelSize; ++i ) {
+        values[ i ] = gauss( i - halfWidth, sigma );
+        sum += values[ i ];
+    }
+
+    // normalize the kernel
+    for( i = 0; i < kernelSize; ++i ) values[ i ] /= sum;
+
+    return values;
+}
